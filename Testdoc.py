ae74a3a82b6566c7b40eccd07ad7f755c3d9180b
Kushal Veerapaneni <137858620+Kushal-0532@users.noreply.github.com>
2023-12-30 20:39:35 +0530

    Update Testdoc.py

--- Testdoc.py
+++ Testdoc.py
@@ -5,22 +5,19 @@ peg=reader.get_object()
 print(peg)
 page = reader.pages[0] 
   '''
-# extracting text from page 
-text = page.extract_text() 
-print(text) 
+
 
 from openai import OpenAI
 
 client = OpenAI(api_key="Put your api key here")
 import PyPDF2
 
-pdfFile = open('Put your pdf destination here')
-pdfReader = PyPDF2.PdfReader(pdfFile)
-pdfText = ''
-for page in pdfReader.pages:
-  pdfText += page.extract_text()
+import PyPDF2
+reader = PyPDF2.PdfReader('put pdf destination here')     
+x=reader.pages[0]   #0 for first page
+textfrmpdf=x.extract_text()
 
-
-response = client.completions.create(model='gpt-3.5-turbo',prompt=f"Read this block of text and ask some questions about it: {pdfText}")
+#Trying to understand this part
+response = client.completions.create(model='gpt-3.5-turbo',prompt="Read this block of text and ask some questions about it: {0}".format(textfrmpdf))
 questions = response["choices"][0]["text"].split("\n")
 print(questions)
